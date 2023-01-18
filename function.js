06d26d18093c5f9c132cb6656b8216c71a87dc52
LautaroAZ <122891351+LautaroAZ@users.noreply.github.com>
2023-01-18 14:13:03 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,20 @@
+
+function msjautomatico(button1) {
+    console.log ("En condiciones optimas debería conducir a la página de Facebook");
+    alert("En condiciones optimas debería conducir a la página de Facebook");
+}
+
+function msjautomatico(button2) {
+    console.log ("En condiciones optimas debería conducir a la página de Twitter");
+    alert("En condiciones optimas debería conducir a la página de Twitter");
+}
+
+function msjautomatico(button3) {
+    console.log ("En condiciones optimas debería conducir a la página de Instagram");
+    alert("En condiciones optimas debería conducir a la página de Instagram");
+}
+
+function msjautomatico(button4) {
+    console.log ("En condiciones optimas debería conducir a la página de LinkedIn");
+    alert("En condiciones optimas debería conducir a la página de LinkedIn");
+}
